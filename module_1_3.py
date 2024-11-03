43184adc46daeae6e7acfbc7c94a4ba04ff30bbf
Rodion792 <duninrodion@gmail.com>
2024-11-03 12:42:17 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,5 @@
+homerwork = 12
+time_spent = 1.5
+course_name = 'Python'
+time_of_one_lesson = time_spent / homerwork
+print('Курс:', course_name + ',', 'всего задачь:', str(homerwork) + ',',  'затрачено часов:', str(time_spent) + ',',  'среднее время выполнения', time_of_one_lesson)
